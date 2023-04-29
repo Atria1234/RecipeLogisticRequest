4eb5ffc36750339765407a195a02c641b67161fe
Tomáš Chmelík <tomas.chmelik2@gmail.com>
2023-04-29 08:38:18 +0200

    Added support for requesting by clicking on items and entities

--- control.lua
+++ control.lua
@@ -1,75 +1,86 @@
-function modify_requests(character, items)
+local infinity = 4294967295
+local max_request_slot = 65536
+
+local function modify_requests(character, requested_items)
     -- modify already existing requests
     for slot = 1, character.request_slot_count do
         local request = character.get_personal_logistic_slot(slot)
-        local requested_item = request.name
-        local requested_count = items[requested_item]
-        if requested_item and requested_count then
+        local requested_count = requested_items[request.name]
+        if request.name ~= nil and requested_count ~= nil then
             if requested_count > 0 then
                 request.min = request.min + requested_count
-                if request.max < 4294967295 then
-                    request.max = request.max + requested_count
-                end
+                request.max = math.min(request.max + requested_count, infinity)
 
                 character.set_personal_logistic_slot(slot, request)
             else
-                if request.min + requested_count <= 0 and (request.max + requested_count <= 0 or request.max == 4294967295) then
+                if request.min + requested_count <= 0 and (request.max + requested_count <= 0 or request.max == infinity) then
                     character.clear_personal_logistic_slot(slot)
                 else
                     request.min = math.max(request.min + requested_count, 0)
-                    if request.max < 4294967295 then
-                        request.max = request.max + requested_count
-                    end
+                    request.max = request.max < infinity and math.min(request.max + requested_count, infinity) or infinity
 
                     character.set_personal_logistic_slot(slot, request)
                 end
             end
-            items[requested_item] = nil
+
+            requested_items[request.name] = nil
         end
     end
 
     -- create new requests
-    local next = next
-    for slot = 1, 65536 do
-        if next(items) == nil then
+    for slot = 1, max_request_slot do
+        if next(requested_items) == nil then
             break
         end
 
         local request = character.get_personal_logistic_slot(slot)
         if request.name == nil then
-            local name, count = next(items)
-            if count > 0 then
+            local requested_name, requested_count = next(requested_items)
+            if requested_count > 0 then
                 character.set_personal_logistic_slot(slot, {
-                    name = name,
-                    min = count,
+                    name = requested_name,
+                    min = requested_count,
                     max = nil
                 })
             end
-            items[name] = nil
+
+            requested_items[requested_name] = nil
         end
     end
 end
 
-function get_recipe_requests(ingredients_or_products, multiplier)
+local function get_recipe_requests(ingredients_or_products, multiplier)
     local items = {}
     for _, item in ipairs(ingredients_or_products) do
         if item.type == "item" then
             items[item.name] = multiplier * (item.amount or 1)
         end
     end
+
     return items
 end
 
-function create_event_handler(use_result, multiplier)
-    function handler(event)
+local function create_event_handler(use_result, multiplier)
+    local function handler(event)
         local player = game.players[event.player_index]
-        if event.selected_prototype and event.selected_prototype.base_type == "recipe" then
-            if player ~= nil and player.valid and player.character ~= nil and player.character.valid then
+        if player ~= nil and player.valid and player.character ~= nil and player.character.valid then
+            if event.selected_prototype and event.selected_prototype.base_type == "recipe" then
                 local recipe = player.character.force.recipes[event.selected_prototype.name]
+                modify_requests(player.character, get_recipe_requests(use_result and recipe.products or recipe.ingredients, multiplier))
+            elseif event.selected_prototype and event.selected_prototype.base_type == "item" then
                 if use_result then
-                    modify_requests(player.character, get_recipe_requests(recipe.products, multiplier))
-                else
-                    modify_requests(player.character, get_recipe_requests(recipe.ingredients, multiplier))
+                    modify_requests(player.character, {
+                        [event.selected_prototype.name] = multiplier
+                    })
+                end
+            elseif event.selected_prototype and event.selected_prototype.base_type == "entity" then
+                if use_result then
+                    local items_to_place_this = game.entity_prototypes[event.selected_prototype.name].items_to_place_this
+                    if items_to_place_this and table_size(items_to_place_this) == 1 then
+                        modify_requests(player.character, {
+                            [items_to_place_this[1].name] = multiplier
+                        })
+                    end
                 end
             end
         end
@@ -80,12 +91,9 @@ end
 
 script.on_event("RecipeLogisticRequest__increase-request-result", create_event_handler(true, 1))
 script.on_event("RecipeLogisticRequest__increase-request-result-5", create_event_handler(true, 5))
-
-script.on_event("RecipeLogisticRequest__decrease-request-result", create_event_handler(true, -1))
-script.on_event("RecipeLogisticRequest__decrease-request-result-5", create_event_handler(true, -5))
-
 script.on_event("RecipeLogisticRequest__increase-request-ingredients", create_event_handler(false, 1))
 script.on_event("RecipeLogisticRequest__increase-request-ingredients-5", create_event_handler(false, 5))
-
+script.on_event("RecipeLogisticRequest__decrease-request-result", create_event_handler(true, -1))
+script.on_event("RecipeLogisticRequest__decrease-request-result-5", create_event_handler(true, -5))
 script.on_event("RecipeLogisticRequest__decrease-request-ingredients", create_event_handler(false, -1))
 script.on_event("RecipeLogisticRequest__decrease-request-ingredients-5", create_event_handler(false, -5))
